fe91a693aed36398094cb6db07887cad071c4ae5
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-09-14 05:38:19 -0400

    add bills page

---
+++ src/app/modules/bills/bills-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BillsComponent } from './pages/bills/bills.component';
+
+const routes: Routes = [
+  {path: '', component: BillsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BillsRoutingModule { }
