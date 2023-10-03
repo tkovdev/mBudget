e84d243b20dd3c0cb2c8af10f6d3823c1558ebee
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-10-03 16:36:33 -0400

    update paid bills sep

--- src/app/modules/bills/pipes/paid.pipe.ts
+++ src/app/modules/bills/pipes/paid.pipe.ts
@@ -6,9 +6,14 @@ import {IBill} from "../../../models/bill.model";
 })
 export class PaidPipe implements PipeTransform {
 
-  transform(value: IBill, paid: boolean): boolean {
-    if(paid) return value.amount != null;
-    else return value.amount == null;
+  transform(value: IBill | IBill[], paid: boolean): boolean {
+    if(Array.isArray(value)){
+      if(paid) return value.some(x => x.amount != null);
+      else return value.some(x => x.amount == null);
+    }else{
+      if(paid) return value.amount != null;
+      else return value.amount == null;
+    }
   }
 
 }
