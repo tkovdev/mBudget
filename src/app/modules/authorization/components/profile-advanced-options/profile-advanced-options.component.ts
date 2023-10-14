d8f4765f2cfc3101569cd852ed6426f8a52a78ed
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-10-14 08:01:22 -0400

    allow manual updating of underlying bills

---
+++ src/app/modules/authorization/components/profile-advanced-options/profile-advanced-options.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
+import {FilesService} from "../../../../services/files.service";
+
+@Component({
+  selector: 'app-profile-advanced-options',
+  templateUrl: './profile-advanced-options.component.html',
+  styleUrls: ['./profile-advanced-options.component.scss']
+})
+export class ProfileAdvancedOptionsComponent {
+  fileId: string;
+  fileContent: string;
+
+  constructor(private config: DynamicDialogConfig, private ref: DynamicDialogRef, private filesService: FilesService) {
+    this.fileId = this.config.data.fileId;
+    this.fileContent = this.config.data.fileContent;
+  }
+
+  saveFile(): void {
+    this.filesService.updateFile(this.fileId, this.fileContent).then(() => {
+      this.ref.close();
+    })
+  }
+}
