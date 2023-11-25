170eed47997f2a545f603ab27d7f00377f10d433
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-11-25 11:28:09 -0500

    display stats on action bar

--- src/app/modules/budgets/components/budget-action-bar/budget-action-bar.component.ts
+++ src/app/modules/budgets/components/budget-action-bar/budget-action-bar.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IBudget} from "../../../../models/budget.model";
 
 @Component({
   selector: 'app-budget-action-bar',
@@ -8,6 +9,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class BudgetActionBarComponent {
   @Input('budgets') budgets: string[] = [];
   @Input('selectedBudget') selectedBudget!: string;
+  @Input('budget') budget!: IBudget;
   @Output() budgetChanged: EventEmitter<void> = new EventEmitter<void>();
   @Output() budgetDeleted: EventEmitter<string> = new EventEmitter<string>();
   @Output() budgetSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
@@ -27,5 +29,12 @@ export class BudgetActionBarComponent {
 
   deleteBudget(name: string): void {
     this.budgetDeleted.emit(name);
+  }
+
+  get remaining(): number {
+    let need = this.budget.breakdown.need.monthlyTotal!;
+    let want = this.budget.breakdown.want.monthlyTotal!;
+    let extra = this.budget.breakdown.extra.monthlyTotal!;
+    return (this.budget.income - (need + want + extra));
   }
 }
